1d6b8f57ac6b5339c913f0d8a78be11ec482f7cb
Ana Gavrilovic <ana.gavrilovic247@gmail.com>
2023-06-24 14:22:16 +0200

    project initialization

---
+++ employment-agency-backend/src/main/java/com/example/employmentagencybackend/EmploymentAgencyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.employmentagencybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentAgencyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploymentAgencyBackendApplication.class, args);
+	}
+
+}
---
+++ employment-agency-backend/src/test/java/com/example/employmentagencybackend/EmploymentAgencyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employmentagencybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentAgencyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
